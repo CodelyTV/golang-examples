5976329c4857be3c655ce41b49838386e5b81011
aperezg <perezg.adrian@gmail.com>
2019-05-02 23:00:13 +0200

    [lesson 01] Evaluating with os.Args

--- 01-go_flags/main.go
+++ 01-go_flags/main.go
@@ -5,7 +5,16 @@ import (
 	"os"
 )
 
+var beers = map[string]string{
+	"01D9X58E7NPXX5MVCR9QN794CH": "Mad Jack Mixer",
+	"01D9X5BQ5X48XMMVZ2F2G3R5MS": "Keystone Ice",
+	"01D9X5CVS1M9VR5ZD627XDF6ND": "Belgian Moon",
+}
+
 func main() {
 	param := os.Args[1]
+	if param == "beers" {
+		fmt.Println(beers)
+	}
 	fmt.Println(param)
 }
