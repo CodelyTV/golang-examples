49368f9d74906da6c7dad442c0e3a9060b187029
aperezg <perezg.adrian@gmail.com>
2019-05-02 23:14:43 +0200

    [lesson 01] Refactor using FlagSet and create our first cmd

--- 01-go_flags/main.go
+++ 01-go_flags/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"os"
 )
 
 var beers = map[string]string{
@@ -12,14 +14,24 @@ var beers = map[string]string{
 }
 
 func main() {
-	beersFlag := flag.Bool("beers", false, "show beers")
-	flag.Usage = func() {
-		fmt.Println("This is a cli create for CodelyTV Go course")
-		flag.PrintDefaults()
-	}
+
+	beersCmd := flag.NewFlagSet("beers", flag.ExitOnError)
 	flag.Parse()
 
-	if *beersFlag {
-		fmt.Println(beers)
+	if flag.NArg() == 0 {
+		log.Fatal("You must specified a command beers")
+		os.Exit(2)
+	}
+
+	switch flag.Arg(0) {
+	case "beers":
+		ID := beersCmd.String("id", "", "find by ID")
+		beersCmd.Parse(os.Args[2:])
+
+		if *ID != "" {
+			fmt.Println(beers[*ID])
+		} else {
+			fmt.Println(beers)
+		}
 	}
 }
