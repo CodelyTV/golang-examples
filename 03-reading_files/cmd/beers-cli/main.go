27439c7bbaced1231efd700a40468c0c1604da5f
aperezg <perezg.adrian@gmail.com>
2019-05-02 23:37:02 +0200

    [lesson 03] Initial commit from lesson 02

---
+++ 03-reading_files/cmd/beers-cli/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/CodelyTV/golang-introduction/03-reading_files/internal/cli"
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	rootCmd := &cobra.Command{Use: "beers-cli"}
+	rootCmd.AddCommand(cli.InitBeersCmd())
+	rootCmd.Execute()
+}
