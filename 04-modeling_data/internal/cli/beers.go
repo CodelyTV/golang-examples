20fb33dc6e581466c5209e8ea387eb5b81cf6217
aperezg <perezg.adrian@gmail.com>
2019-05-03 08:46:38 +0200

    [lesson 04] refactoring using defined struct

--- 04-modeling_data/internal/cli/beers.go
+++ 04-modeling_data/internal/cli/beers.go
@@ -7,6 +7,7 @@ import (
 	"strconv"
 	"strings"
 
+	beerscli "github.com/CodelyTV/golang-introduction/04-modeling_data/internal"
 	"github.com/spf13/cobra"
 )
 
@@ -33,13 +34,25 @@ func runBeersFn() CobraFn {
 		f, _ := os.Open("03-reading_files/data/beers.csv")
 		reader := bufio.NewReader(f)
 
-		var beers = make(map[int]string)
+		var beers = make(map[int]beerscli.Beer)
 
 		for line := readLine(reader); line != nil; line = readLine(reader) {
 			values := strings.Split(string(line), ",")
 
 			productID, _ := strconv.Atoi(values[0])
-			beers[productID] = values[1]
+			price, _ := strconv.ParseFloat(values[3], 64)
+
+			beer := beerscli.Beer{
+				ProductID: productID,
+				Name:      values[1],
+				Category:  values[2],
+				Price:     price,
+				Type:      values[4],
+				Brewer:    values[5],
+				Country:   values[6],
+			}
+
+			beers[productID] = beer
 		}
 
 		id, _ := cmd.Flags().GetString(idFlag)
