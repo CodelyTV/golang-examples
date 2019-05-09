60e736b597f457fb58613492c5a2e48e6e2e71a3
aperezg <perezg.adrian@gmail.com>
2019-05-09 21:43:50 +0200

    [lesson 08] using sub tests, TableDrivenTests

--- 08-automated_tests/internal/fetching/service_test.go
+++ 08-automated_tests/internal/fetching/service_test.go
@@ -1,23 +1,38 @@
 package fetching
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/CodelyTV/golang-introduction/08-automated_tests/internal/storage/inmem"
 )
 
 func TestFetchByID(t *testing.T) {
+	tests := map[string]struct {
+		input int
+		want  int
+		err   error
+	}{
+		"valid beer":     {input: 127, want: 127, err: nil},
+		"not found beer": {input: 99999, err: errors.New("error")},
+	}
 	repo := inmem.NewRepository()
-
 	service := NewService(repo)
 
-	expected := 127
-	b, err := service.FetchByID(expected)
-	if err != nil {
-		t.Fatalf("expected %d, got an error %v", expected, err)
-	}
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			b, err := service.FetchByID(tc.input)
+			if err != nil && tc.err == nil {
+				t.Fatalf("not expected any errors and got %v", err)
+			}
 
-	if b.ProductID != expected {
-		t.Fatalf("expected %d,  got: %d ", expected, b.BeerID)
+			if err == nil && tc.err != nil {
+				t.Error("expected an error and got nil")
+			}
+
+			if b.ProductID != tc.want {
+				t.Fatalf("expected %d,  got: %d ", tc.want, b.ProductID)
+			}
+		})
 	}
 }
