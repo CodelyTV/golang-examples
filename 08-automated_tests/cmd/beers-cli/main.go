1434b1e70398245ef816206052948ada9215b3e7
aperezg <perezg.adrian@gmail.com>
2019-05-09 19:07:23 +0200

    [lesson 08] Initial commit from lesson 07

---
+++ 08-automated_tests/cmd/beers-cli/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"flag"
+
+	"github.com/CodelyTV/golang-introduction/08-automated_tests/internal/storage/ontario"
+
+	beerscli "github.com/CodelyTV/golang-introduction/08-automated_tests/internal"
+	"github.com/CodelyTV/golang-introduction/08-automated_tests/internal/cli"
+	"github.com/CodelyTV/golang-introduction/08-automated_tests/internal/storage/csv"
+	"github.com/spf13/cobra"
+)
+
+func main() {
+
+	csvData := flag.Bool("csv", false, "load data from csv")
+	flag.Parse()
+	var repo beerscli.BeerRepo
+	repo = csv.NewRepository()
+
+	if !*csvData {
+		repo = ontario.NewOntarioRepository()
+	}
+
+	rootCmd := &cobra.Command{Use: "beers-cli"}
+	rootCmd.AddCommand(cli.InitBeersCmd(repo))
+	rootCmd.Execute()
+}
---
+++ 08-automated_tests/internal/cli/beers.go
@@ -0,0 +1,53 @@
+package cli
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/CodelyTV/golang-introduction/08-automated_tests/internal/errors"
+
+	beerscli "github.com/CodelyTV/golang-introduction/08-automated_tests/internal"
+	"github.com/spf13/cobra"
+)
+
+// CobraFn function definion of run cobra command
+type CobraFn func(cmd *cobra.Command, args []string)
+
+const idFlag = "id"
+
+// InitBeersCmd initialize beers command
+func InitBeersCmd(repository beerscli.BeerRepo) *cobra.Command {
+	beersCmd := &cobra.Command{
+		Use:   "beers",
+		Short: "Print data about beers",
+		Run:   runBeersFn(repository),
+	}
+
+	beersCmd.Flags().StringP(idFlag, "i", "", "id of the beer")
+
+	return beersCmd
+}
+
+func runBeersFn(repository beerscli.BeerRepo) CobraFn {
+	return func(cmd *cobra.Command, args []string) {
+		beers, err := repository.GetBeers()
+		if errors.IsDataUnreacheable(err) {
+			log.Fatal(err)
+		}
+
+		id, _ := cmd.Flags().GetString(idFlag)
+
+		if id != "" {
+			i, _ := strconv.Atoi(id)
+			for _, beer := range beers {
+				if beer.ProductID == i {
+					fmt.Println(beer)
+					return
+				}
+			}
+		} else {
+			fmt.Println(beers)
+		}
+	}
+}
