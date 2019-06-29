cd17994693f64f3a89b9f9346c606e7c96a733a7
jr.sanchez <jr.sanchez@appgree.com>
2019-06-29 17:40:40 +0200

    repository.go: Refactored some code to a get common function

--- 08-automated_tests/internal/storage/ontario/repository.go
+++ 08-automated_tests/internal/storage/ontario/repository.go
@@ -30,14 +30,9 @@ func NewOntarioRepository() (beerscli.BeerRepo, beerscli.StoreRepo) {
 }
 
 func (b *beerRepo) GetBeers() (beers []beerscli.Beer, err error) {
-	response, err := http.Get(fmt.Sprintf("%v%v", b.url, productsEndpoint))
+	contents, err := get(b.url, productsEndpoint)
 	if err != nil {
-		return nil, errors.WrapDataUnreacheable(err, "error getting response to %s", productsEndpoint)
-	}
-
-	contents, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		return nil, errors.WrapDataUnreacheable(err, "error reading the response from %s", productsEndpoint)
+		return nil, err
 	}
 
 	err = json.Unmarshal(contents, &beers)
@@ -48,14 +43,9 @@ func (b *beerRepo) GetBeers() (beers []beerscli.Beer, err error) {
 }
 
 func (b *storeRepo) GetStores() (stores []beerscli.Store, err error) {
-	response, err := http.Get(fmt.Sprintf("%v%v", b.url, storesEndpoint))
+	contents, err := get(b.url, storesEndpoint)
 	if err != nil {
-		return nil, errors.WrapDataUnreacheable(err, "error getting response to %s", storesEndpoint)
-	}
-
-	contents, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		return nil, errors.WrapDataUnreacheable(err, "error reading the response from %s", storesEndpoint)
+		return nil, err
 	}
 
 	err = json.Unmarshal(contents, &stores)
@@ -63,4 +53,18 @@ func (b *storeRepo) GetStores() (stores []beerscli.Store, err error) {
 		return nil, errors.WrapDataUnreacheable(err, "can't parsing response into stores")
 	}
 	return
+}
+
+func get(url, endpoint string) ([]byte, error) {
+	response, err := http.Get(fmt.Sprintf("%v%v", url, endpoint))
+	if err != nil {
+		return nil, errors.WrapDataUnreacheable(err, "error getting response to %s", endpoint)
+	}
+
+	contents, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, errors.WrapDataUnreacheable(err, "error reading the response from %s", endpoint)
+	}
+
+	return contents, nil
 }
