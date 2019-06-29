1ecdc396c9189724b6c2134538ed12a00262d3d3
jr.sanchez <jr.sanchez@appgree.com>
2019-06-29 17:21:36 +0200

    Fix float32 latitude and longitude to string

--- 08-automated_tests/internal/store.go
+++ 08-automated_tests/internal/store.go
@@ -2,27 +2,27 @@ package beerscli
 
 // Store representation of store into data struct
 type Store struct {
-	StoreID        int     `json:"store_id"`
-	Name           string  `json:"name"`
-	Address        string  `json:"address"`
-	City           string  `json:"city"`
-	PostalCode     string  `json:"postal_code"`
-	Latitude       float32 `json:"latitude"`
-	Longitude      float32 `json:"longitude"`
-	MondayOpen     string  `json:"monday_open"`
-	MondayClose    string  `json:"monday_close"`
-	TuesdayOpen    string  `json:"tuesday_open"`
-	TuesdayClose   string  `json:"tuesday_close"`
-	WednesdayOpen  string  `json:"wednesday_open"`
-	WednesdayClose string  `json:"wednesday_close"`
-	ThursdayOpen   string  `json:"thusday_open"`
-	ThursdayClose  string  `json:"thursday_close"`
-	FridayOpen     string  `json:"friday_open"`
-	FridayClose    string  `json:"friday_close"`
-	SaturdayOpen   string  `json:"saturday_open"`
-	SaturdayClose  string  `json:"saturday_close"`
-	SundayOpen     string  `json:"sunday_open"`
-	SundayClose    string  `json:"sunday_close"`
+	StoreID        int    `json:"store_id"`
+	Name           string `json:"name"`
+	Address        string `json:"address"`
+	City           string `json:"city"`
+	PostalCode     string `json:"postal_code"`
+	Latitude       string `json:"latitude"`
+	Longitude      string `json:"longitude"`
+	MondayOpen     string `json:"monday_open"`
+	MondayClose    string `json:"monday_close"`
+	TuesdayOpen    string `json:"tuesday_open"`
+	TuesdayClose   string `json:"tuesday_close"`
+	WednesdayOpen  string `json:"wednesday_open"`
+	WednesdayClose string `json:"wednesday_close"`
+	ThursdayOpen   string `json:"thusday_open"`
+	ThursdayClose  string `json:"thursday_close"`
+	FridayOpen     string `json:"friday_open"`
+	FridayClose    string `json:"friday_close"`
+	SaturdayOpen   string `json:"saturday_open"`
+	SaturdayClose  string `json:"saturday_close"`
+	SundayOpen     string `json:"sunday_open"`
+	SundayClose    string `json:"sunday_close"`
 }
 
 // StoreRepo definiton of methods to access a data store
@@ -33,7 +33,7 @@ type StoreRepo interface {
 // NewStore initialize struct store
 func NewStore(storeID int,
 	name, address, city, postalCode string,
-	latitude, longitude float32,
+	latitude, longitude,
 	mondayOpen, mondayClose, tuesdayOpen,
 	tuesdayClose, wednesdayOpen, wednesdayClose,
 	thursdayOpen, thursdayClose, fridayOpen,
