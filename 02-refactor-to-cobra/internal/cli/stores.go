3cb1759dee61925ea1c703303b7c97c57875e004
Rolando Caldas <rolando.caldas@gmail.com>
2019-10-13 17:16:07 +0200

    Added stores command

---
+++ 02-refactor-to-cobra/internal/cli/stores.go
@@ -0,0 +1,38 @@
+package cli
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var stores = map[string]string{
+	"01DC9ZAPGKEQJS4P4A48EG3P43": "Mercadona",
+	"01DC9ZB23EW0J0ARAER09SJDKC": "Carrefour",
+	"01DC9ZB89V1PQD977ZE6QXSQHH": "Alcampo",
+}
+
+// InitBeersCmd initialize beers command
+func InitStoresCmd() *cobra.Command {
+	storesCmd := &cobra.Command{
+		Use:   "stores",
+		Short: "Print where you can purchase the beers",
+		Run:   runStoresFn(),
+	}
+
+	storesCmd.Flags().StringP(idFlag, "i", "", "id of the beer")
+
+	return storesCmd
+}
+
+func runStoresFn() CobraFn {
+	return func(cmd *cobra.Command, args []string) {
+		id, _ := cmd.Flags().GetString(idFlag)
+
+		if id != "" {
+			fmt.Println(stores[id])
+		} else {
+			fmt.Println(stores)
+		}
+	}
+}
