9b5f4bee4dc10d4722752ec9f4b87d112e57db2b
Joel Coll Costa <joel.coll@gmail.com>
2020-06-26 09:16:07 +0200

    Format code with gofmt

--- 02-refactor-to-cobra/cmd/beers-cli/main.go
+++ 02-refactor-to-cobra/cmd/beers-cli/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	rootCmd := &cobra.Command{Use: "beers-cli"}
-    rootCmd.AddCommand(cli.InitBeersCmd())
-    rootCmd.AddCommand(cli.InitStoresCmd())
-    rootCmd.Execute()
+	rootCmd.AddCommand(cli.InitBeersCmd())
+	rootCmd.AddCommand(cli.InitStoresCmd())
+	rootCmd.Execute()
 }
--- 02-refactor-to-cobra/internal/cli/stores.go
+++ 02-refactor-to-cobra/internal/cli/stores.go
@@ -9,9 +9,9 @@ import (
 type CobraStoresFn func(cmd *cobra.Command, args []string)
 
 var stores = map[string]string{
-    "01DC9ZAPGKEQJS4P4A48EG3P43": "Mercadona",
-    "01DC9ZB23EW0J0ARAER09SJDKC": "Carrefour",
-    "01DC9ZB89V1PQD977ZE6QXSQHH": "Alcampo",
+	"01DC9ZAPGKEQJS4P4A48EG3P43": "Mercadona",
+	"01DC9ZB23EW0J0ARAER09SJDKC": "Carrefour",
+	"01DC9ZB89V1PQD977ZE6QXSQHH": "Alcampo",
 }
 
 const idStoresFlag = "id"
