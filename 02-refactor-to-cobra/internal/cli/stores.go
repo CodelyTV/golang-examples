b12456dc7495ac5fe221f8f848d96a8042dfacd4
Richard Abel Avendaño Pillaca <abl.avn@gmail.com>
2020-12-15 09:39:46 -0500

    reto stores

---
+++ 02-refactor-to-cobra/internal/cli/stores.go
@@ -0,0 +1,30 @@
+package cli
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var stores = map[string]string{"01DC9ZAPGKEQJS4P4A48EG3P43": "Mercadona", "01DC9ZB23EW0J0ARAER09SJDKC": "Carrefour", "01DC9ZB89V1PQD977ZE6QXSQHH": "Alcampo"}
+
+func InitStoresCmd() *cobra.Command {
+	storesCmd := &cobra.Command{
+		Use:   "stores",
+		Short: "Print stores",
+		Run:   runStoresFn(),
+	}
+	storesCmd.Flags().StringP(idFlag, "i", "", "id of store")
+	return storesCmd
+}
+
+func runStoresFn() CobraFn {
+	return func(cmd *cobra.Command, args []string) {
+		id, _ := cmd.Flags().GetString(idFlag)
+		if id != "" {
+			fmt.Println(stores[id])
+		} else {
+			fmt.Println(stores)
+		}
+	}
+}
