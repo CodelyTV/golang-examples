4269d8942d2a4850062afec6703180388cd0d5d7
David Callizaya <davidcallizaya@gmail.com>
2021-03-29 17:04:09 -0400

    Fix CSV flag

--- 07-behaviour_error_handling/cmd/beers-cli/main.go
+++ 07-behaviour_error_handling/cmd/beers-cli/main.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"flag"
-
 	"github.com/CodelyTV/golang-examples/07-behaviour_error_handling/internal/storage/ontario"
 
-	beerscli "github.com/CodelyTV/golang-examples/07-behaviour_error_handling/internal"
 	"github.com/CodelyTV/golang-examples/07-behaviour_error_handling/internal/cli"
 	"github.com/CodelyTV/golang-examples/07-behaviour_error_handling/internal/storage/csv"
 	"github.com/spf13/cobra"
@@ -13,16 +10,10 @@ import (
 
 func main() {
 
-	csvData := flag.Bool("csv", false, "load data from csv")
-	flag.Parse()
-	var repo beerscli.BeerRepo
-	repo = csv.NewRepository()
-
-	if !*csvData {
-		repo = ontario.NewOntarioRepository()
-	}
+	repoAPI := csv.NewRepository()
+	repoCSV := ontario.NewOntarioRepository()
 
 	rootCmd := &cobra.Command{Use: "beers-cli"}
-	rootCmd.AddCommand(cli.InitBeersCmd(repo))
+	rootCmd.AddCommand(cli.InitBeersCmd(repoAPI, repoCSV))
 	rootCmd.Execute()
 }
