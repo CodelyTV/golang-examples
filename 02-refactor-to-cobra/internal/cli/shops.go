9a7b17d5a4a4ae4ccbde6ba6fd097fbc6455347b
Didweb <info@did-web.com>
2021-07-25 14:51:24 +0200

    Shops

---
+++ 02-refactor-to-cobra/internal/cli/shops.go
@@ -0,0 +1,42 @@
+package cli
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// CobraFn function definion of run cobra command
+type CobraShopsFn func(cmd *cobra.Command, args []string)
+
+var shops = map[string]string{
+	"01DC9ZAPGKEQJS4P4A48EG3P43": "Mercadona",
+	"01DC9ZB23EW0J0ARAER09SJDKC": "Carrefour",
+	"01DC9ZB89V1PQD977ZE6QXSQHH": "Alcampo",
+}
+
+const idShopsFlag = "id"
+
+func InitShopsCmd() *cobra.Command {
+	shopsCmd := &cobra.Command{
+		Use:   "shops",
+		Short: "Print data about shops",
+		Run:   runShopsFn(),
+	}
+
+	shopsCmd.Flags().StringP(idFlag, "i", "", "id of the shop")
+
+	return shopsCmd
+}
+
+func runShopsFn() CobraShopsFn {
+	return func(cmd *cobra.Command, args []string) {
+		id, _ := cmd.Flags().GetString(idFlag)
+
+		if id != "" {
+			fmt.Println(shops[id])
+		} else {
+			fmt.Println(shops)
+		}
+	}
+}
