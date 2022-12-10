26bcc87c9664e857f78c70b9460d3b675995f9af
brianlewyn <brianlewyn@gmail.com>
2022-12-09 19:29:16 -0600

    Adding stores command

--- 02-refactor-to-cobra/internal/cli/beers.go
+++ 02-refactor-to-cobra/internal/cli/beers.go
@@ -6,22 +6,11 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// CobraFn function definion of run cobra command
-type CobraFn func(cmd *cobra.Command, args []string)
-
 var beers = map[string]string{
 	"01D9X58E7NPXX5MVCR9QN794CH": "Mad Jack Mixer",
 	"01D9X5BQ5X48XMMVZ2F2G3R5MS": "Keystone Ice",
 	"01D9X5CVS1M9VR5ZD627XDF6ND": "Belgian Moon",
 }
-
-var stores = map[string]string{
-	"01DC9ZAPGKEQJS4P4A48EG3P43": "Mercadona",
-	"01DC9ZB23EW0J0ARAER09SJDKC": "Carrefour",
-	"01DC9ZB89V1PQD977ZE6QXSQHH": "Alcampo",
-}
-
-const idFlag = "id"
 
 // InitBeersCmd initialize beers command
 func InitBeersCmd() *cobra.Command {
@@ -44,31 +33,6 @@ func runBeersFn() CobraFn {
 			fmt.Println(beers[id])
 		} else {
 			fmt.Println(beers)
-		}
-	}
-}
-
-// InitStoresCmd initialize Stores command
-func InitStoresCmd() *cobra.Command {
-	storesCmd := &cobra.Command{
-		Use:   "stores",
-		Short: "Print data about stores",
-		Run:   runStoresFn(),
-	}
-
-	storesCmd.Flags().StringP(idFlag, "i", "", "id of the store")
-
-	return storesCmd
-}
-
-func runStoresFn() CobraFn {
-	return func(cmd *cobra.Command, args []string) {
-		id, _ := cmd.Flags().GetString(idFlag)
-
-		if id != "" {
-			fmt.Println(stores[id])
-		} else {
-			fmt.Println(stores)
 		}
 	}
 }
