b0b265665d58345b4bc9b49d43e6cdb29ef155af
brianlewyn <brianlewyn@gmail.com>
2022-12-09 19:20:55 -0600

    Fork of the 02-refactor-a-cobra

--- 02-refactor-to-cobra/internal/cli/beers.go
+++ 02-refactor-to-cobra/internal/cli/beers.go
@@ -15,6 +15,12 @@ var beers = map[string]string{
 	"01D9X5CVS1M9VR5ZD627XDF6ND": "Belgian Moon",
 }
 
+var stores = map[string]string{
+	"01DC9ZAPGKEQJS4P4A48EG3P43": "Mercadona",
+	"01DC9ZB23EW0J0ARAER09SJDKC": "Carrefour",
+	"01DC9ZB89V1PQD977ZE6QXSQHH": "Alcampo",
+}
+
 const idFlag = "id"
 
 // InitBeersCmd initialize beers command
@@ -38,6 +44,31 @@ func runBeersFn() CobraFn {
 			fmt.Println(beers[id])
 		} else {
 			fmt.Println(beers)
+		}
+	}
+}
+
+// InitStoresCmd initialize Stores command
+func InitStoresCmd() *cobra.Command {
+	storesCmd := &cobra.Command{
+		Use:   "stores",
+		Short: "Print data about stores",
+		Run:   runStoresFn(),
+	}
+
+	storesCmd.Flags().StringP(idFlag, "i", "", "id of the store")
+
+	return storesCmd
+}
+
+func runStoresFn() CobraFn {
+	return func(cmd *cobra.Command, args []string) {
+		id, _ := cmd.Flags().GetString(idFlag)
+
+		if id != "" {
+			fmt.Println(stores[id])
+		} else {
+			fmt.Println(stores)
 		}
 	}
 }
