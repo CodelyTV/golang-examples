cd4380a07569aedf265f9055285a125c0482540e
Xisco Bibiloni <neo.xisco@gmail.com>
2023-01-04 10:09:56 +0100

    Revert "feat:New store command"
    This reverts commit aef753fe48e359f2865dca888c3d1e014eb848bc.

--- 02-refactor-to-cobra/internal/cli/beers.go
+++ 02-refactor-to-cobra/internal/cli/beers.go
@@ -1,8 +1,13 @@
 package cli
 
 import (
+	"fmt"
+
 	"github.com/spf13/cobra"
 )
+
+// CobraFn function definion of run cobra command
+type CobraFn func(cmd *cobra.Command, args []string)
 
 var beers = map[string]string{
 	"01D9X58E7NPXX5MVCR9QN794CH": "Mad Jack Mixer",
@@ -10,15 +15,29 @@ var beers = map[string]string{
 	"01D9X5CVS1M9VR5ZD627XDF6ND": "Belgian Moon",
 }
 
+const idFlag = "id"
+
 // InitBeersCmd initialize beers command
 func InitBeersCmd() *cobra.Command {
 	beersCmd := &cobra.Command{
 		Use:   "beers",
 		Short: "Print data about beers",
-		Run:   runCommandFn(beers),
+		Run:   runBeersFn(),
 	}
 
-	beersCmd.Flags().StringP(idFlag, shortIdFlag, "", "id of the beer")
+	beersCmd.Flags().StringP(idFlag, "i", "", "id of the beer")
 
 	return beersCmd
+}
+
+func runBeersFn() CobraFn {
+	return func(cmd *cobra.Command, args []string) {
+		id, _ := cmd.Flags().GetString(idFlag)
+
+		if id != "" {
+			fmt.Println(beers[id])
+		} else {
+			fmt.Println(beers)
+		}
+	}
 }
